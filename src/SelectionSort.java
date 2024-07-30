27771c49168df948fc56ba9e341c17d2fcc7edfc
Akhil <akhilpgvr@gmail.com>
2024-07-30 23:40:52 +0530

    added selectionSort algorithm

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,5 +1,18 @@
 public class SelectionSort {
     public int[] performSelectionSort(int[] arr, int size) {
-        return new int[]{};
+
+        for(int i=0;i<size;i++) {
+
+            int minIndex = i;
+            for(int j=i+1;i<size;i++) {
+                if(arr[minIndex]>arr[j]) minIndex = j;
+            }
+            if(minIndex!=i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+        return arr;
     }
 }
