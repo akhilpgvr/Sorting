68aa0e42800156fd4b7d45cec48e8da31ada02d5
Akhil <akhilpgvr@gmail.com>
2024-07-31 12:38:28 +0530

    added selectionSort algorithm

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,50 @@ public class Main {
         int arrSize = arr.length;
 
         System.out.println("...........................");
-        System.out.print("Array is: ");
-        for(int i: arr) {
-            System.out.print(i+ " ");
-        }
-        System.out.println();
-        System.out.println("...........................");
         System.out.println("Bubble Sorting");
         System.out.println("...........................");
         BubbleSort bubbleSort = new BubbleSort();
+        System.out.print("Array is: ");
+        bubbleSort.printSortedArray(arr);
+        System.out.println();
         arr = bubbleSort.performBubbleSort(arr, arrSize);
         bubbleSort.printSortedArray(arr);
         System.out.println("...........................");
+        System.out.println("...........................");
         System.out.println("Selection Sorting");
         System.out.println("...........................");
+        arr = new int[]{ 7, 2, 13, 11,3, 9, 1, 8, 6};
         SelectionSort selectionSort = new SelectionSort();
+        System.out.print("Array is: ");
+        selectionSort.printArray(arr);
+        System.out.println();
         arr = selectionSort.performSelectionSort(arr, arrSize);
-        bubbleSort.printSortedArray(arr);
+        selectionSort.printArray(arr);
         System.out.println("...........................");
         System.out.println("...........................");
     }
 }
+
+/*
+
+Output:
+...........................
+Bubble Sorting
+...........................
+Array is: Sorted array:
+3 9 1 8 6 7 2 13 11
+
+Sorted array:
+1 2 3 6 7 8 9 11 13
+...........................
+...........................
+Selection Sorting
+...........................
+Array is: Sorted array:
+7 2 13 11 3 9 1 8 6
+
+Sorted array:
+1 2 3 6 7 8 9 11 13
+...........................
+...........................
+ */
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,10 +1,10 @@
 public class SelectionSort {
     public int[] performSelectionSort(int[] arr, int size) {
 
-        for(int i=0;i<size;i++) {
+        for(int i=0;i<size-1;i++) {
 
             int minIndex = i;
-            for(int j=i+1;i<size;i++) {
+            for(int j=i+1;j<size;j++) {
                 if(arr[minIndex]>arr[j]) minIndex = j;
             }
             if(minIndex!=i) {
@@ -15,4 +15,14 @@ public class SelectionSort {
         }
         return arr;
     }
+
+    public void printArray(int[] arr) {
+
+        System.out.println("Sorted array: ");
+        for(int ele: arr) {
+            System.out.print(ele+ " ");
+        }
+        System.out.println();
+    }
+
 }
